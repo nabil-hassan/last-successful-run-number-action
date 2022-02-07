99b8d05916fb6334f7267d52f907f5e9e988ff2a
Nabil Hassan <nabil.hassan@clearchannel.co.uk>
2022-02-07 00:02:08 +0000

    ...

--- dist/index.js
+++ dist/index.js
@@ -8474,17 +8474,23 @@ const github = __nccwpck_require__(5438);
 try {
     const octo = github.getOctokit(core.getInput("token"));
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    const workflow_id = core.getInput("workflow_id");
+    const branch = core.getInput("branch");
+
+    console.log("Fetching workflow runs for workflow: ${workflow_id} and branch: ${branch}.");
+    
     octo.rest.actions.listWorkflowRuns({
         owner, repo,
-        workflow_id: core.getInput("workflow_id"),
+        workflow_id: workflow_id,
         status: "success",
-        branch: core.getInput("branch"),
-        per_page: 1
+        branch: branch,
+        per_page: 1,
     }).then((res) => {
         const run_number = res.data.workflow_runs.length > 0
             ? res.data.workflow_runs[0].run_number : "";
         core.setOutput("run_number", run_number);
     }).catch((e) => {
+        console.log("Unable to fetch workflow run number");
         core.setFailed(e.message);
     });
 } catch (e) {
